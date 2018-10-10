a26521c6b1ea90272a9be6ceefe3c2ac6162071d
Umith Indula Wimalarathna <umithindula213@gmail.com>
2018-10-10 05:31:38 -0700

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -4,3 +4,15 @@ if(Input.lower() == Reverse.lower()):
     print('String is Palindrome')
 else:
     print('String is not Palindrome')
+quickSort(arr[], low, high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);  // Before pi
+        quickSort(arr, pi + 1, high); // After pi
+    }
+}
