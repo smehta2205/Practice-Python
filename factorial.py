80c11b5f77d325494617efb70617908473ad90c6
Swapnil Buchke <sbuchke085@gmail.com>
2018-10-10 11:15:20 +0530

    factorial without using recursion

--- factorial.py
+++ factorial.py
@@ -1,15 +1,10 @@
-def recur_factorial(n):
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
-
-
-num = input()
-num = int(num)
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
+n=int(input("Enter number:"))
+fact=1
+if n==0:
+    print(fact)
 else:
-   print("The factorial of",num,"is",recur_factorial(num))
+    while(n>0):
+        fact=fact*n
+        n=n-1
+    print("Factorial of the number is: ")
+    print(fact)
