13faf800c41bc04221469d323201d5903dace273
utkucanbykl <utkucanbykl@gmail.com>
2018-10-11 10:28:44 +0300

    Add fibonacci py

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(count):
+    fib = [0, 1]
+    for x in range(9):
+        fib.append(fib[::-1][0] + fib[::-1][1])
+    return fib
+
+count = input("How many numbers generate in fibonacci")
+print(fibonacci(count))
+print(one_line_fibonacci(count))
+
