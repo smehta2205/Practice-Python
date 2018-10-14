2e3950b9a7e7a7ce6cb5e8c4e5cf10a6de12972b
vidyadeepa <44111486+vidyadeepa@users.noreply.github.com>
2018-10-14 23:37:20 +0530

    Add files via upload

--- reverse.py
+++ reverse.py
@@ -1,5 +1,7 @@
-s = input().split()
-l = list(s)
-#print (l)
-for i in range(len(l)-1,-1,-1):
-    print (l[i],end=" ")
+def reverse(string):
+    if len(string) == 0:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
+a = str(input("Enter the string to be reversed: "))
+print(reverse(a))
